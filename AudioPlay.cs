ce2c717513a20494f506b79dea2f5037a566c097
Steve Gargolinski <steve.gargolinski@gmail.com>
2014-05-18 19:41:25 -0400

    Adds AudioPlay.cs with bug fixes.

---
+++ AudioPlay.cs
@@ -0,0 +1,118 @@
+// (c) Copyright HutongGames, LLC 2010-2013. All rights reserved.
+
+using UnityEngine;
+
+namespace HutongGames.PlayMaker.Actions
+{
+	[ActionCategory(ActionCategory.Audio)]
+	[Tooltip("Plays the Audio Clip set with Set Audio Clip or in the Audio Source inspector on a Game Object. Optionally plays a one shot Audio Clip.")]
+	public class AudioPlay : FsmStateAction
+	{
+		[RequiredField]
+		[CheckForComponent(typeof(AudioSource))]
+		[Tooltip("The GameObject with an AudioSource component.")]
+		public FsmOwnerDefault gameObject;
+		
+		[HasFloatSlider(0,1)]
+        [Tooltip("Set the volume.")]
+		public FsmFloat volume;
+		
+		[ObjectType(typeof(AudioClip))]
+		[Tooltip("Optionally play a 'one shot' AudioClip. NOTE: Volume cannot be adjusted while playing a 'one shot' AudioClip.")]
+		public FsmObject oneShotClip;
+		
+		[Tooltip("Event to send when the AudioClip finishes playing.")]
+		public FsmEvent finishedEvent;
+
+		private AudioSource audio;
+		
+		// SPGADD
+		private float countdownTime;
+				
+		public override void Reset()
+		{
+			gameObject = null;
+			volume = 1f;
+			oneShotClip = null;
+		    finishedEvent = null;
+			countdownTime = -1.0f;
+		}
+
+		public override void OnEnter()
+		{
+			var go = Fsm.GetOwnerDefaultTarget(gameObject);
+			if (go != null)
+			{
+				// cache the AudioSource component
+				
+				audio = go.audio;
+				if (audio != null)
+				{
+					var audioClip = oneShotClip.Value as AudioClip;
+
+					if (audioClip == null)
+					{
+						audio.Play();
+						
+						if (!volume.IsNone)
+						{
+							audio.volume = volume.Value;
+						}
+						
+						return;
+					}
+					
+					if (!volume.IsNone)
+					{
+						audio.PlayOneShot(audioClip, volume.Value);
+					}
+					else
+					{
+						audio.PlayOneShot(audioClip);
+					}
+					
+					// SPGADD
+					countdownTime = audioClip.length;
+						
+					return;
+				}
+			}
+			
+			// Finish if failed to play sound
+			Finish();
+		}
+		
+		public override void OnUpdate ()
+		{
+			if (audio == null)
+			{
+				Finish();
+			}
+			else
+			{
+				// SPGADD
+				if (countdownTime > 0.0f)
+				{
+					countdownTime -= Time.deltaTime;
+					if (countdownTime <= 0.0f)
+					{
+						Fsm.Event(finishedEvent);
+						Finish();
+					}
+				}
+			
+				// SPGCOMMENTEDOUT
+				/*
+				if (!audio.isPlaying)
+				{
+					Fsm.Event(finishedEvent);
+					Finish();
+				}
+                else */if (!volume.IsNone && volume.Value != audio.volume)
+				{
+					audio.volume = volume.Value;
+				}
+			}
+		}
+	}
+}
